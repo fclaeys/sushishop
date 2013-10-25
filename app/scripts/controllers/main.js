d509e135219d3078aba3cf21c58bb27d40ff1da8
Fabrice Claeys <claeys.fabrice@gmail.com>
2013-10-25 13:15:17 +0200

    config du gitignore pour les projets idea

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,11 @@
 
 angular.module('sushishopApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.catalogue = [{ name: "Sushi thon", price: 5},
+        {name:"Sushi plethore", price: 7},
+        {name:"Sushi saumon", price:6},
+        {name:"Sushi thon rouge", price:6}];
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
