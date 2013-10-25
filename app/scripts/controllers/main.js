34451397261b54cdabd6a0c934387bbde9ced01c
Fabrice Claeys <claeys.fabrice@gmail.com>
2013-10-25 13:45:06 +0200

    gere la quantite et le prix total

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,18 @@ angular.module('sushishopApp')
     $scope.shoppingCart = [];
 
     $scope.addToCart = function(sushi){
-        $scope.shoppingCart.push(sushi);
+        var inShoppingCart = $scope.shoppingCart.filter(function(elt){
+            return elt.name == sushi.name;
+        });
+
+        if(inShoppingCart.length == 0){
+            var itemToAdd= angular.copy(sushi);
+            angular.extend(itemToAdd, {quantity: 1});
+            $scope.shoppingCart.push(itemToAdd);
+        } else {
+            inShoppingCart[0].price+=sushi.price;
+            inShoppingCart[0].quantity+=1;
+        }
     }
 
     $scope.awesomeThings = [
