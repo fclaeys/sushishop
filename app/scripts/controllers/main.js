3b1de1c6463c3833aed36fadb54d3792ee332b6f
Benjamin Barbier <benjamin.barbier@dark-sides.com>
2013-10-27 22:59:21 +0100

    Update IHM + Add sushi pictures

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,12 @@
 
 angular.module('sushishopApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.catalogue = [{ name: "Sushi thon", price: 5},
-        {name:"Sushi plethore", price: 7},
-        {name:"Sushi saumon", price:6},
-        {name:"Sushi thon rouge", price:6}];
+    $scope.catalogue = [
+        {name:"Sushi thon", price: 5, img: "img_sushi_thon.png"},
+        {name:"Sushi plethore", price: 7, img: "img_sushi_plethore.png"},
+        {name:"Sushi saumon", price:6, img: "img_sushi_saumon.png"},
+        {name:"Sushi thon rouge", price:6, img: "img_sushi_thon_rouge.png"},
+        {name:"Sushi quirielle", price:16, img: "img_sushi_quirielle.png"}];
 
     $scope.shoppingCart = [];
 
