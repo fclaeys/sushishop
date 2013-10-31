4b6e5322bc1072c975e712f361d11b1e8929155b
Fabrice Claeys <claeys.fabrice@gmail.com>
2013-10-31 13:23:15 +0100

    injection du catalogService

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,8 @@
 'use strict';
 
 angular.module('SushishopApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.catalogue = [
-        {name:"Sushi thon", price: 5, img: "img_sushi_thon.png"},
-        {name:"Sushi plethore", price: 7, img: "img_sushi_plethore.png"},
-        {name:"Sushi saumon", price:6, img: "img_sushi_saumon.png"},
-        {name:"Sushi thon rouge", price:6, img: "img_sushi_thon_rouge.png"},
-        {name:"Sushi quirielle", price:16, img: "img_sushi_quirielle.png"}];
+  .controller('MainCtrl', function ($scope, Catalogservice) {
+    $scope.catalogue = Catalogservice.getSushies();
 
     $scope.shoppingCart = [];
 
